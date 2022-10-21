7638f65502eb366f1aac39bacd72117a19c57ac6
Huf <bjhufstetler@gmail.com>
2022-10-20 21:22:46 -0700

    Adds password

--- ui/src/components/Login/InstructorLogin.tsx
+++ ui/src/components/Login/InstructorLogin.tsx
@@ -1,13 +1,29 @@
 import "../../App.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const InstructorLogin = () => {
+    const code = "a"; // INSTRUCTOR PASSWORD
     const navigate = useNavigate();
+
+    const [password, setPassword] = useState<string>("");
+    const [auth, setAuth] = useState<boolean>(false);
+
+    const handleChangePassword = (e: any) => {
+        const tmpPassword = e.target.value
+        setPassword(tmpPassword)
+        if (tmpPassword == code) {
+            setAuth(true);
+        }else{
+            setAuth(false);
+        };
+    };
+
     return(
         <div className="Card">
-            <input type="text" placeholder="PASSWORD"></input>
-            <button onClick={()=> navigate("/instructorCreate")}>CREATE</button>
-            <button onClick={()=> navigate("/instructorJoin")}>JOIN</button>
+            <input type="text" placeholder="PASSWORD" value={password} onChange={e => handleChangePassword(e)}></input>
+            <button onClick={()=> auth?navigate("/instructorCreate"):{}}>CREATE</button>
+            <button onClick={()=> auth?navigate("/instructorJoin"):{}}>JOIN</button>
             <button onClick={() => navigate("/")}>BACK</button>
         </div>
     )
