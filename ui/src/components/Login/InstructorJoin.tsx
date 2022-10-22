f788dfce4afa33acbb477975d1888cd377bed097
Huf <bjhufstetler@gmail.com>
2022-10-22 12:14:33 -0700

    Adds server dependence to instructor page

--- ui/src/components/Login/InstructorJoin.tsx
+++ ui/src/components/Login/InstructorJoin.tsx
@@ -1,17 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEwokContext } from "../../context/EwokContext";
 
 const StudentLogin = () => {
     const navigate = useNavigate();
+    const { ewok, setEwok } = useEwokContext();
+    const [server, setServer] = useState<string>("");
+    const handleChangeServer = (e: any) => {
+        setServer(e.target.value)
+    }
+    const handleClickJoin = () => {
+        const tmpEwok = {...ewok, server: server}
+        setEwok(tmpEwok)
+        navigate("/instructor")
+    }
     return(
         <div className="Card">
             <div>
                 SERVER ID
-                <select id="team">
-                    <option value="0000">0000</option>
-                    <option value="0001">0001</option>
+                <select onChange={(e) => handleChangeServer(e)}>
+                    {// TODO: Get server options from db
+                    }
+                    <option disabled selected></option>
+                    <option value="0a2b">'0a2b'</option>
+                    <option value="0a2c">'0a2c'</option>
                 </select>
             </div>
-            <button onClick={() => navigate("/instructor")}>JOIN</button>
+            
+            <button onClick={() => handleClickJoin()}>JOIN</button> 
             <button onClick={() => navigate("/instructorLogin")}>BACK</button>
         </div>
     )
