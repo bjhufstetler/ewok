f102ff355f947de0a28ccdedca5ea9f5f6a935fe
bjhufstetler <bjhufstetler@gmail.com>
2023-03-27 22:21:50 -0700

    Fixes TX user inputs

--- ui/src/components/Student/Transmitter.tsx
+++ ui/src/components/Student/Transmitter.tsx
@@ -3,6 +3,12 @@ import { useEwokContext, useEquipmentContext } from "../../context/EwokContext";
 import './Transmitter.css'
 
 const Transmitter = () => {
+    // LIMITS
+    const minFrequency = 800;
+    const maxFrequency = 2200;
+    const maxDataRate = 60;
+    const maxPower = -50
+
     const { equipment } = useEquipmentContext();
     const { socket } = useEwokContext();
     
@@ -28,15 +34,11 @@ const Transmitter = () => {
     const handlePlusPowerButton = (e: any) => {
         const tmpSettings = {
             ...settings,
-            power: settings.power + 1
+            power: Math.min(tx.power + 1, maxPower)
         };
         setSettings(tmpSettings);
         const tmpEquipment = {
             ...tx,
-            cf: tmpSettings.cf,
-            dr: tmpSettings.dr,
-            mod: tmpSettings.mod,
-            fec: tmpSettings.fec,
             power: tmpSettings.power
         }
         socket.emit('PATCH', 'equipment', tmpEquipment)
@@ -46,10 +48,10 @@ const Transmitter = () => {
                 server: tx.server,
                 conn: tx.conn,
                 team: tx.team,
-                cf: tmpSettings.cf,
-                dr: tmpSettings.dr,
-                mod: tmpSettings.mod,
-                fec: tmpSettings.fec,
+                cf: tx.cf,
+                dr: tx.dr,
+                mod: tx.mod,
+                fec: tx.fec,
                 power: tmpSettings.power,
                 band: tmpAntenna.unit_name,
                 sat: tmpAntenna.sat,
@@ -64,16 +66,12 @@ const Transmitter = () => {
 
     const handleMinusPowerButton = (e: any) => {
         const tmpSettings = {
-            ...settings,
-            power: settings.power - 1
+            ...tx,
+            power: tx.power - 1
         };
         setSettings(tmpSettings);
         const tmpEquipment = {
             ...tx,
-            cf: tmpSettings.cf,
-            dr: tmpSettings.dr,
-            mod: tmpSettings.mod,
-            fec: tmpSettings.fec,
             power: tmpSettings.power
         }
         socket.emit('PATCH', 'equipment', tmpEquipment)
@@ -83,10 +81,10 @@ const Transmitter = () => {
                 server: tx.server,
                 conn: tx.conn,
                 team: tx.team,
-                cf: tmpSettings.cf,
-                dr: tmpSettings.dr,
-                mod: tmpSettings.mod,
-                fec: tmpSettings.fec,
+                cf: tx.cf,
+                dr: tx.dr,
+                mod: tx.mod,
+                fec: tx.fec,
                 power: tmpSettings.power,
                 band: tmpAntenna.unit_name,
                 sat: tmpAntenna.sat,
@@ -102,21 +100,21 @@ const Transmitter = () => {
     const handleChangeCF = (e: any) => {
         const tmpSettings = {
             ...settings,
-            cf: Number(e.target.value)
+            cf: e.target.value
         };
         setSettings(tmpSettings);
     };
     const handleChangeDR = (e: any) => {
         const tmpSettings = {
             ...settings,
-            dr: Number(e.target.value)
+            dr: e.target.value
         };
         setSettings(tmpSettings);
     };
     const handleChangePower = (e: any) => {
         const tmpSettings = {
             ...settings,
-            power: Number(e.target.value)
+            power: e.target.value
         };
         setSettings(tmpSettings);
     };
@@ -128,6 +126,7 @@ const Transmitter = () => {
         };
         setSettings(tmpSettings);
     };
+
     const handleChangeMod = (e: any) => {
         const tmpSettings = {
             ...settings,
@@ -138,13 +137,32 @@ const Transmitter = () => {
 
 
     const handleClickUpdate = () => {
+        const cf = Number(settings.cf);
+        const dr = Number(settings.dr);
+        const power = Number(settings.power);
+
+        // Error handling for bad TX values
+        if ( isNaN(cf + dr + power) ) {
+            alert('Invalid TX value.')
+            return;
+        } else if ( cf < minFrequency || cf > maxFrequency ) {
+            alert('Frequency is outside of supported range.');
+            return;
+        } else if ( dr > maxDataRate ) {
+            alert('Data rate exceeds limits.');
+            return;
+        } else if ( power > maxPower ) {
+            alert('Power exceeds limits.')
+            return;
+        };
+        
         const tmpEquipment = {
             ...tx,
-            cf: settings.cf,
-            dr: settings.dr,
+            cf: cf,
+            dr: dr,
             mod: settings.mod,
             fec: settings.fec,
-            power: settings.power
+            power: power
         }
         socket.emit('PATCH', 'equipment', tmpEquipment)
         if(tx.active){
@@ -153,11 +171,11 @@ const Transmitter = () => {
                 server: tx.server,
                 conn: tx.conn,
                 team: tx.team,
-                cf: settings.cf,
-                dr: settings.dr,
+                cf: cf,
+                dr: dr,
                 mod: settings.mod,
                 fec: settings.fec,
-                power: settings.power,
+                power: power,
                 band: tmpAntenna.unit_name,
                 sat: tmpAntenna.sat,
                 feed: tx.feed,
@@ -265,10 +283,10 @@ const Transmitter = () => {
                 </div>       
                 <div className='txModemOutputs'>
                     <span className='label'>Frequency:</span>    
-                    <input type='text' value={settings?.cf} onChange={e => handleChangeCF(e)}></input>
+                    <input className={isNaN(settings?.cf) || settings?.cf < minFrequency || settings.cf > maxFrequency? "invalid" : ""} type='text' value={settings?.cf} onChange={e => handleChangeCF(e)}></input>
                     <span className='unit'>MHz</span>
                     <span className='label'>Data Rate:</span>    
-                    <input type='text' value={settings?.dr} onChange={e => handleChangeDR(e)}></input>
+                    <input type='text' className={isNaN(settings?.dr) || settings?.dr > maxDataRate ? "invalid" : ""} value={settings?.dr} onChange={e => handleChangeDR(e)}></input>
                     <span className='unit'>MHz</span>
                     <span className='label'>Modulation:</span>    
                     <select value={settings?.mod} onChange={e => handleChangeMod(e)}>
@@ -284,7 +302,7 @@ const Transmitter = () => {
                     </select>
                     <span className='unit'></span>
                     <span className='label'>Power:</span>    
-                    <input type='text' value={settings?.power} /*onKeyDown={e=>handleKeyDown(e)}*/ onChange={e => handleChangePower(e)}></input>
+                    <input className={isNaN(settings?.power) || settings.power > maxPower ? "invalid" : ""} type='text' value={settings?.power} /*onKeyDown={e=>handleKeyDown(e)}*/ onChange={e => handleChangePower(e)}></input>
                     <span className='unit'>dB</span>
                     <span></span>
                     <button onClick={() => handleClickUpdate()}>Update</button>
