93a34e7925dbf9dd0acd40f66414e006d42a84c1
Seth Finley <finley.seth.9@gmail.com>
2023-04-02 08:44:49 -0700

    StartStop works, took setIR out of button

--- ui/src/components/Page/ClockModule/ScenarioClock.tsx
+++ ui/src/components/Page/ClockModule/ScenarioClock.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import { useEwokContext } from "../../../context/EwokContext";
+import '../Header.css';
+
+// TODO: Find a way to get around the async nature of use State such that the correct boolean will emit on start/stop
+
+
 
 const ScenarioClock = () => {
+    let time = new Date().toUTCString();
 
     const {socket, ewok} = useEwokContext();
     const [isRunning, setIsRunning] = useState<boolean>(false);
+    const [scenarioTime, setScenarioTime] = useState<string>(time);
 
     // This is just to show the EWOK context current details at each page for debug purposes
     console.log("Server: "+ewok.server)
@@ -18,10 +25,6 @@ const ScenarioClock = () => {
     //    TYPE: StartStop or ClockSet (dT calc'd locally)
     //    If StartStop, then will have isRunning var, true if start, false if stop
     //    If ClockSet, then will have time to set it to, then will have time
-    //    IR: true or false for is running or not
-    //    ClockSet: true or false for whether or not clock is being set
-    //    StartStop: string of "start" or "stop" to denote what to do
-    //    dT: number or something for amount to adjust zulu time by
     //}
 
     // Handlers
@@ -29,26 +32,29 @@ const ScenarioClock = () => {
         // alert("The string was "+update.message);
         // console.log(update); 
         if (update.type === "StartStop") {
-            //alert('StartStop type object returned.');
-            if (update.runningBool) {setIsRunning(true)}
-            else if (!update.runningBool) {setIsRunning(false)}
+            setIsRunning(update.runningBool);
+            alert('Setting isRunning to '+update.runningBool.toString())
         }
         else if (update.type === "ClockSet") {alert('ClockSet type object returned.')}
         else {alert('Neither type detected.')}
 
     };
+    const updateTime = () => {
+        let time = new Date().toUTCString();
+        setScenarioTime(time);
+    };
     const handleSetButton = (e:any) => {
         e.preventDefault();
         socket.emit('ScenarioClock',{type:"ClockSet"});
     }
-    const handleStartButton = (e:any) => {
+    const handleStartStopButton = (e:any) => {
         e.preventDefault();
-        socket.emit('ScenarioClock',{type:"StartStop",runningBool:true});
+        socket.emit('ScenarioClock',{type:"StartStop",runningBool:!isRunning});
     }
-    const handleStopButton = (e:any) => {
-        e.preventDefault();
-        socket.emit('ScenarioClock',{type:"StartStop",runningBool:false});
-    }
+    // const handleStopButton = (e:any) => {
+    //     e.preventDefault();
+    //     socket.emit('ScenarioClock',{type:"StartStop",runningBool:false});
+    // }
 
     // Handle Clock Status Update
     useEffect(() => {
@@ -58,23 +64,32 @@ const ScenarioClock = () => {
         } // This was returning the alert case twice, so adding socket.off as seen solved the problem
     }, [socket]);
 
+    // Handle Clock Display increments
+    useEffect(()=> {
+        let inte:any;
+        if (isRunning) {
+            inte = setInterval(updateTime, 1000/*ms*/);
+        }
+        else if (!isRunning) {
+            return clearInterval(inte);
+        }
+    },[isRunning])
+
     if (ewok.team === "Instructor") {
         return (
-            <div>
-                <div className="ScenarioClockText">Running: {isRunning.toString()}</div>
+            <div className="ScenarioClock">
+                <div className="ScenarioClockTime">Scenario Time: {scenarioTime.substring(17,25)} {isRunning.toString()}</div>
                 <div className="ScenarioClockButtons">
                     <div className="ScenarioClockButtonSet"><button onClick={handleSetButton}>Set</button></div>
-                    <div className="ScenarioClockButtonStart"><button onClick={handleStartButton}>Start</button></div>
-                    <div className="ScenarioClockButtonStop"><button onClick={handleStopButton}>Stop</button></div>
+                    <div className="ScenarioClockButtonStartStop"><button onClick={handleStartStopButton}>{ isRunning ? 'Stop' : 'Start' }</button></div>
                 </div>
             </div>
         )
     }
     else {
         return (
-            <div>
-                <div className="ScenarioClockText">Running: {isRunning.toString()}</div>
-            </div>
+            <div className="ScenarioClockTime">Scenario Time: {scenarioTime.substring(17,25)} {isRunning.toString()}</div>
+            
         )
     }
 }
