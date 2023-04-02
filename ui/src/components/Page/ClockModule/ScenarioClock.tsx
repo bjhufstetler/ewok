fd88c9eeaa5ab29678b3196fdd3709963255139f
Seth Finley <finley.seth.9@gmail.com>
2023-04-02 12:46:54 -0700

    SavePush for counter method of time

--- ui/src/components/Page/ClockModule/ScenarioClock.tsx
+++ ui/src/components/Page/ClockModule/ScenarioClock.tsx
@@ -8,31 +8,52 @@ import '../Header.css';
 
 
 const ScenarioClock = () => {
-    let time = new Date().toUTCString();
+    //let time = new Date().toUTCString();
     let intervalID:any;
 
     const {socket, ewok} = useEwokContext();
     const [isRunning, setIsRunning] = useState<boolean>(false);
-    const [scenarioTime, setScenarioTime] = useState<string>(time);
+    const [scenarioTime, setScenarioTime] = useState<number>(0);
+    const [editTimeShow, setEditTimeShow] = useState<boolean>(false);
 
     // This is just to show the EWOK context current details at each page for debug purposes
     console.log("Server: "+ewok.server)
     console.log("BaseURL: "+ewok.baseURL)
     console.log("Team: "+ewok.team)
 
+    // Edit Popup Form
+    const TimeSetForm = () => {
+        return (
+            <div>
+                <div className="timeInput">
+                    <input placeholder="HH"></input>:
+                    <input placeholder="MM"></input>:
+                    <input placeholder="SS"></input>
+                </div>
+                <div className="SetButton">
+                    <button onClick={handleSetButton}>Set</button>
+                </div>
+            </div>
+        );
+    }
+
     // Handlers
     const handleClockUpdate = (update: any) => {
-        if (update.type === "StartStop") {setIsRunning(update.runningBool);}
+        if (update.type === "StartStop") {setIsRunning(update.runningBool);setScenarioTime(update.timeKey)}
         else if (update.type === "ClockSet") {alert('ClockSet type object returned.')}
         else {alert('Neither type detected.')}
     };
     const handleEditButton = (e:any) => {
         e.preventDefault();
+        setEditTimeShow(!editTimeShow)
+    }
+    const handleSetButton = () => {
+        setEditTimeShow(false);
         socket.emit('ScenarioClock',{type:"ClockSet"});
     }
     const handleStartStopButton = (e:any) => {
         e.preventDefault();
-        socket.emit('ScenarioClock',{type:"StartStop",runningBool:!isRunning});
+        socket.emit('ScenarioClock',{type:"StartStop",runningBool:!isRunning,timeKey:scenarioTime});
     }
 
     // Handle Clock Status Update
@@ -45,8 +66,8 @@ const ScenarioClock = () => {
 
     // Handle Clock Display increments
     const updateTime = () => {
-        let time = new Date().toUTCString();
-        setScenarioTime(time);
+        //let time = new Date().toUTCString();
+        setScenarioTime(scenarioTime === 86399 ? 0 : scenarioTime => scenarioTime+1);
     };
     useEffect(()=> {
         if (isRunning) {intervalID = setInterval(updateTime, 1000/*ms*/);}
@@ -56,17 +77,19 @@ const ScenarioClock = () => {
     if (ewok.team === "Instructor") {
         return (
             <div className="ScenarioClock">
-                <div className="ScenarioClockTime">Scenario Time: {scenarioTime.substring(17,25)} {isRunning.toString()}</div>
+                <div className="ScenarioClockTime">Scenario Time: {scenarioTime.toString()} {isRunning.toString()}</div>
                 <div className="ScenarioClockButtons">
-                    <div className="ScenarioClockButtonSet"><button onClick={handleEditButton}>Set</button></div>
+                    <div className="ScenarioClockButtonSet"><button onClick={handleEditButton}>Edit</button>{editTimeShow.toString()}</div>
                     <div className="ScenarioClockButtonStartStop"><button onClick={handleStartStopButton}>{ isRunning ? 'Stop' : 'Start' }</button></div>
                 </div>
+                <div className="timeInput">{editTimeShow ? <TimeSetForm /> : null}</div>
             </div>
         )
     }
+    //scenarioTime.substring(17,25)
     else {
         return (
-            <div className="ScenarioClockTime">Scenario Time: {scenarioTime.substring(17,25)} {isRunning.toString()}</div>
+            <div className="ScenarioClockTime">Scenario Time: {scenarioTime.toString()} {isRunning.toString()}</div>
             
         )
     }
